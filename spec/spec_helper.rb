2e98f007c3d366e4e2b60b0409ddd3603c9543de
VAILLANT Jeremy <jeremy@dazzl.tv>
2021-05-04 17:47:08 +0200

    Configure simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,21 @@
+# frozen_string_literal: true
+
 require 'mongoid-rspec-callbacks'
 require 'models/user'
+require 'simplecov'
+require 'simplecov_json_formatter'
+
+SimpleCov.start
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
 RSpec.configure do |config|
+  # Stop when rspec fail
+  config.fail_fast = true
+
+  # Exclude spec broken
+  config.filter_run_excluding broken: true
+
   config.include Mongoid::Matchers
 end
